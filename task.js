a47a73e78e763aadb25fa50d8a5844a532c64e8b
RyosukeSomeya <rx.78.0618@gmail.com>
2021-06-12 12:14:10 +0900

    id名修正,タスク追加方法を修正

--- task.js
+++ task.js
@@ -1,16 +1,16 @@
-const addBtn = document.getElementById('addTask');
+const addBtn = document.getElementById('add-task');
 const taskLists = [];
 
 addBtn.addEventListener('click', () => {
     // get form input value
-    const inputValue = document.getElementById('newTask').value;
+    const inputValue = document.getElementById('new-task').value;
     const task = {
         comment: inputValue,
         state: 'wip'
     }
     taskLists.push(task);
     // form reset
-    document.getElementById('newTask').value = '';
+    document.getElementById('new-task').value = '';
     // show table row
     if (taskLists.length > 0) {
         showTask(taskLists);
@@ -19,22 +19,32 @@ addBtn.addEventListener('click', () => {
 
 
 function showTask(taskLists) {
-    const targetElem = document.getElementById('tableBody');
+    const targetElem = document.getElementById('table-body');
     let rowElement;
+
     taskLists.forEach((task, index) => {
         let btnValue = '完了';
         if (task.state === 'wip') {
             btnValue = '作業中';
         }
-        rowElement = `
-            <tr>
-                <td>${index}</td>
-                <td>${task.comment}</td>
-                <td>
-                    <button value="${task.state}">${btnValue}</button>
-                    <button>削除</button>
-                </td>
-            </tr>`;
+        // Create table row elements.
+        rowElement = document.createElement('tr');
+        const idCell = document.createElement('td');
+        const commentCell = document.createElement('td');
+        const stateCell = document.createElement('td');
+        const stateBtn = document.createElement('button');
+        const deleteBtn = document.createElement('button');
+
+        // Set contents to row elements.
+        idCell.innerText = index;
+        commentCell.innerText = task.comment;
+        stateBtn.innerText = btnValue;
+        deleteBtn.innerText = '削除';
+        stateCell.appendChild(stateBtn);
+        stateCell.appendChild(deleteBtn);
+        rowElement.appendChild(idCell);
+        rowElement.appendChild(commentCell);
+        rowElement.appendChild(stateCell);
     });
-    targetElem.insertAdjacentHTML('beforeEnd', rowElement);
+    targetElem.appendChild(rowElement);
 }
